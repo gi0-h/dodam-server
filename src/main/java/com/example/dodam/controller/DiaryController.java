26b0beedb4423a492c7f2702030c8f8aace896a7
13wjdgk <13wjdgk@naver.com>
2023-01-31 21:16:05 +0900

    checkout

--- src/main/java/com/example/dodam/controller/DiaryController.java
+++ src/main/java/com/example/dodam/controller/DiaryController.java
@@ -1,4 +1,13 @@
 package com.example.dodam.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class DiaryController {
+
+    @GetMapping("/")
+    public String home(){
+        return "hello";
+    }
 }
--- src/main/java/com/example/dodam/service/DiaryService.java
+++ src/main/java/com/example/dodam/service/DiaryService.java
@@ -1,7 +1,6 @@
 package com.example.dodam.service;
 
 public class DiaryService {
-    //djfdklfjdlsfjdls
-    //testtesttestest
-    //ㅠㅠㅠ 왜 깃허브 연결 안되냐고 ㅠㅠㅠㅠ
+
+
 }
