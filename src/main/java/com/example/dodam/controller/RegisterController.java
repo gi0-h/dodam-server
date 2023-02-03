279a40ca5f5a3e2ba16717ee40a844dac6dbe7a3
kimminsu <xjfcnfw3@naver.com>
2023-02-03 13:15:11 +0900

    Sign up after verification

--- src/main/java/com/example/dodam/controller/RegisterController.java
+++ src/main/java/com/example/dodam/controller/RegisterController.java
@@ -1,11 +1,18 @@
 package com.example.dodam.controller;
 
+import com.example.dodam.domain.sms.dto.SmsResponse;
+import com.example.dodam.domain.sms.dto.VerificationRequest;
+import com.example.dodam.domain.sms.dto.VerificationResponse;
 import com.example.dodam.domain.user.RegisterRequest;
 import com.example.dodam.domain.user.UserResponse;
 import com.example.dodam.service.FileUploadService;
 import com.example.dodam.service.RegisterService;
+import com.example.dodam.service.SmsService;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import java.io.IOException;
 import java.net.MalformedURLException;
+import java.net.URISyntaxException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.UrlResource;
 import org.springframework.http.MediaType;
@@ -15,20 +22,39 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequestMapping("user/register")
 public class RegisterController {
 
     private final RegisterService registerService;
     private final FileUploadService fileUploadService;
+    private final SmsService smsService;
 
-    public RegisterController(RegisterService registerService, FileUploadService fileUploadService) {
+    public RegisterController(RegisterService registerService, FileUploadService fileUploadService,
+        SmsService smsService) {
         this.registerService = registerService;
         this.fileUploadService = fileUploadService;
+        this.smsService = smsService;
+    }
+
+    @PostMapping("/sms")
+    public ResponseEntity<SmsResponse> getSms(@RequestParam String phone)
+        throws URISyntaxException, JsonProcessingException {
+        SmsResponse response = smsService.sendSms(phone);
+        return ResponseEntity.ok(response);
+    }
+
+    @PostMapping("/verify")
+    public ResponseEntity<VerificationResponse> verifyNumber(@RequestBody VerificationRequest request) {
+        log.debug("request={} {}", request.getCode(), request.getPhone());
+        VerificationResponse response = smsService.verifyCode(request.getPhone(), request.getCode());
+        return ResponseEntity.ok(response);
     }
 
     @PostMapping
@@ -36,6 +62,7 @@ public class RegisterController {
         String imagePath = fileUploadService.storeFile(request.getProfileImage());
         request.setImagePath(imagePath);
         try {
+            smsService.checkPhone(request.getPhone());
             return ResponseEntity.ok(UserResponse.of(registerService.register(request)));
         } catch (Exception e) {
             fileUploadService.deleteFile(imagePath);
