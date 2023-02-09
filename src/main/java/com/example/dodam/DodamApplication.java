aba215a9f1b6b5e636a6ee1b654c8d95ebee4d56
DonghyeonKang <donghyeon3372@gmail.com>
2023-02-09 19:09:52 +0900

    Update build.gradle

--- src/main/java/com/example/dodam/DodamApplication.java
+++ src/main/java/com/example/dodam/DodamApplication.java
@@ -2,10 +2,16 @@ package com.example.dodam;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
-@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
+@SpringBootApplication
 public class DodamApplication {
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(DodamApplication.class, args);
