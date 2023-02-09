43db862748c047e5625e0d4a33baef6a02077493
DonghyeonKang <donghyeon3372@gmail.com>
2023-02-09 19:11:31 +0900

    Create User Entity

--- src/main/java/com/example/dodam/domain/user/User.java
+++ src/main/java/com/example/dodam/domain/user/User.java
@@ -1,9 +1,11 @@
 package com.example.dodam.domain.user;
 
-import javax.persistence.*;
-
 import lombok.Data;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
