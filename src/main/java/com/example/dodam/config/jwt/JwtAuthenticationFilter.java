fa742b4e05458619127d657d3d990c6850e06c87
DonghyeonKang <donghyeon3372@gmail.com>
2023-02-09 19:13:27 +0900

    JwtAuthenticationFilter

--- src/main/java/com/example/dodam/config/jwt/JwtAuthenticationFilter.java
+++ src/main/java/com/example/dodam/config/jwt/JwtAuthenticationFilter.java
@@ -1,26 +1,23 @@
 package com.example.dodam.config.jwt;
 
-import java.io.IOException;
-import java.util.Date;
-
-import javax.servlet.FilterChain;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.example.dodam.config.auth.PrincipalDetails;
+import com.example.dodam.domain.login.LoginRequestDto;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-import com.auth0.jwt.JWT;
-import com.auth0.jwt.algorithms.Algorithm;
-import com.example.dodam.config.auth.PrincipalDetails;
-import com.example.dodam.domain.login.LoginRequestDto;
-import com.fasterxml.jackson.databind.ObjectMapper;
-
-import lombok.RequiredArgsConstructor;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Date;
 
 @RequiredArgsConstructor
 public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilter{
